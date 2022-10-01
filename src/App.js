4654575270c540f5df9b9fe92e94f57a40af61df
iretoms <iretomiwajonathan@gmail.com>
2022-09-30 17:38:04 -0700

    home as privateroute

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/home/Home"
+import PrivateRoute from "./components/PrivateRoute"
 import SignUp from "./pages/sign-up/SignUp";
 import SignIn from "./pages/sign-in/SignIn"
 import ForgotPassword from "./pages/forgot-password/ForgotPassword";
@@ -10,7 +11,9 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/" element={<Home/>}/>
+        </Route>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
