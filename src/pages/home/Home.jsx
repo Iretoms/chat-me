249e8505aececfe4d47b3a0432ed5b178e7513aa
iretoms <iretomiwajonathan@gmail.com>
2022-10-02 22:49:47 -0700

    home page header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
+import "./Home.scss";
+import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+
 const Home = () => {
-    return (
-        <>
-            <header></header>
-            <main></main>
-            <footer></footer>
-        </>
-    );
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  const onLogOut = () => {
+    auth.signOut();
+    navigate("/sign-in");
+  };
+
+  return (
+    <>
+      <header className="homeHeader">
+        <nav className="homeNav">
+          <div className="userInfo">
+            <figure className="userImageContainer">
+              <img
+                src={auth.currentUser.photoURL ? auth.currentUser.photoURL : ""}
+                alt="profile"
+                className="userImage"
+              />
+            </figure>
+            <p className="userName">Welcome {auth.currentUser.displayName}</p>
+          </div>
+          <button className="logOutBtn" onClick={onLogOut}>
+            Log out
+          </button>
+        </nav>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
 };
 
 export default Home;
