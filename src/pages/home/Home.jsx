04e00463a1c7965a33be19d7db2ad9eef6ef88e4
iretoms <iretomiwajonathan@gmail.com>
2022-10-04 15:38:27 -0700

    message input

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,21 @@
 import "./Home.scss";
-import profileImage from "../../assets/png/profileImage.png"
+import { useState } from "react";
+import profileImage from "../../assets/png/profileImage.png";
 import { useNavigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 
 const Home = () => {
+  const [message, setMessage] = useState("");
   const navigate = useNavigate();
   const auth = getAuth();
 
   const onLogOut = () => {
     auth.signOut();
     navigate("/sign-in");
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -19,13 +25,17 @@ const Home = () => {
           <div className="userInfo">
             <figure className="userImageContainer">
               <img
-                src={auth.currentUser.photoURL ? auth.currentUser.photoURL : profileImage}
+                src={
+                  auth.currentUser.photoURL
+                    ? auth.currentUser.photoURL
+                    : profileImage
+                }
                 alt="profile"
                 className="userImage"
               />
             </figure>
             <div>
-              <p className="userName">Welcome</p>
+              <p className="userName">Welcome,</p>
               <p className="userName">{auth.currentUser.displayName}</p>
             </div>
           </div>
@@ -34,8 +44,27 @@ const Home = () => {
           </button>
         </nav>
       </header>
-      <main></main>
-      <footer></footer>
+      <main>
+        <div className="chatContainer">
+
+        </div>
+      </main>
+      <footer className="homeFooter">
+        <div className="inputContainer">
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              className="messageInput"
+              placeholder="Write a message..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button type="submit" className="logOutBtn">
+              Send
+            </button>
+          </form>
+        </div>
+      </footer>
     </>
   );
 };
